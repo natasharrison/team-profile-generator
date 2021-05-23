188457a34d97193b1d97839f3fac2cb413fe2186
natasharrison <natasharrison@gmail.com>
2021-05-23 09:29:58 -0700

    styling

--- index.js
+++ index.js
@@ -6,114 +6,98 @@ const Profile = require('./lib/Profile');
 const generateHtml = require('./dist/generateHtml');
 
 
-const managerQuestions = [
-	{
-		type: 'input',
-		name: 'name',
-		message: 'First and last name of your team manager:'
-	},
-    {
-		type: 'input',
-		name: 'id',
-		message: "Team manager's ID number:"
-	},
-	{
-		type: 'input',
-		name: 'email',
-		message: "Team manager's email address:"
-	},
-	{
-		type: 'input',
-		name: 'office',
-		message: "Team manager's office number:"
-	}
-];
-
-// presented with menu that has options to add an engineer or intern or finish building team
-
-const engineerQuestions = [
-	{	
+const questions = [
+  {
     type: 'input',
-		name: 'name',
-		message: 'First and last name of team engineer:'
-	},
-    {
-		type: 'input',
-		name: 'id',
-		message: "Team engineer's ID number:"
-	},
-	{
-		type: 'input',
-		name: 'email',
-		message: "Team engineer's email address:"
-	},
-	{
-		type: 'input',
-		name: 'github',
-		message: "Team engineer's GitHub username:"
-	}
-];
-
-// presented with menu that has options to add an engineer or intern or finish building team
-
-const internQuestions = [
-	{	
+    name: 'name',
+    message: 'First and last name of your team member:'
+  },
+  {
     type: 'input',
-		name: 'name',
-		message: 'First and last name of team intern:'
-	},
-    {
-		type: 'input',
-		name: 'id',
-		message: "Team intern's ID number:"
-	},
-	{
-		type: 'input',
-		name: 'email',
-		message: "Team intern's email address:"
-	},
-	{
-		type: 'input',
-		name: 'github',
-		message: "Team intern's school:"
-	}
+    name: 'id',
+    message: "Team member's ID number:"
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Team member's email address:"
+  },
+  {
+    type: 'list',
+    name: 'role',
+    message: 'What type of role would you like to add?',
+    choices: ["manager", "engineer", "intern"]
+  },
+  // only for manager 
+  {
+    type: 'input',
+    name: 'office',
+    message: "Team manager's office number:"
+  },
+  // only for engineer
+  {
+    type: 'input',
+    name: 'github',
+    message: "Team engineer's GitHub username:"
+  },
+  // only for intern
+  {
+    type: 'input',
+    name: 'github',
+    message: "Team intern's school name:"
+  }
 ];
 // presented with menu that has options to add an engineer or intern or finish building team
+
+// function to write HTML file using the user input
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function initializeProfile() {
+  inquirer.prompt(questions).then(inquirerResponses => {
+    console.log('Generating HTML...');
+    writeToFile('profile.html', generateHtml({ ...inquirerResponses }));
+  });
+};
 
 const generateHtml = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
+  return `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Team</title>
-  </head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="team-profile-generator/dist/style.css" />
+</head>
 
-  <body>
-    <h1>${name}</h1>
-    <h2>${title}</h2>
-    <p>ID #: ${id}</p>
-    <p><a href="mailto:${email}">Email: ${email}</a></p>
-    <p>Office #: </p>
-    <p><a href="https://github.com/${github}">Github</a></p>
-    <p>School: </p>
-  </body>
-  </html>
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
+
+    <main class="m-5 row justify-content-around">
+        <div class="col-3">
+            <div class="card">
+                <h3 class="card-header">${name}</h3>
+                <br>
+                <div class="card-body">
+                    <p>${title}</p>
+                    <p>ID #: ${id}</p>
+                    <p><a href="mailto:${email}">Email: ${email}</a></p>
+                    <p>Office #: </p>
+                    <p><a href="https://github.com/${github}">Github</a></p>
+                    <p>School: </p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
   `;
-};
-
-// function to write HTML file using the user input
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
-
-function initializeProfile(){
-    inquirer.prompt(managerQuestions).then(inquirerResponses => {
-        console.log('Generating HTML...');
-        writeToFile('profile.html', generateHtml({ ...inquirerResponses}));
-    });
 };
 
 initializeProfile();
