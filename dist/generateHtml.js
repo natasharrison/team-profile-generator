6150584870412d6c741f0304f3aa39a38b445a2c
natasharrison <natasharrison@gmail.com>
2021-05-23 09:54:28 -0700

    inital question structure

--- dist/generateHtml.js
+++ dist/generateHtml.js
@@ -1,5 +1,40 @@
-function generateHtml(data){
-    
+function generateHtml(data) {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="team-profile-generator/dist/style.css" />
+</head>
+
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
+
+    <main class="m-5 row justify-content-around">
+        <div class="col-3">
+            <div class="card">
+                <h3 class="card-header">${data.name}</h3>
+                <br>
+                <div class="card-body">
+                    <p>${data.role}</p>
+                    <p>ID #: ${data.id}</p>
+                    <p><a href="mailto:${data.email}">Email: ${data.email}</a></p>
+                    <p>Office #: ${data.office}</p>
+                    <p><a href="https://github.com/${data.github}">Github</a></p>
+                    <p>School: ${data.school} </p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+  `;
 }
 
 module.exports = generateHtml;
--- index.js
+++ index.js
@@ -43,61 +43,30 @@ const questions = [
   // only for intern
   {
     type: 'input',
-    name: 'github',
+    name: 'school',
     message: "Team intern's school name:"
+  }, 
+  {
+    type:'confirm', 
+    name: 'repeat',
+    message: "Would you like to enter another team member?"
   }
+  // if yes, repeat questions 
 ];
-// presented with menu that has options to add an engineer or intern or finish building team
 
-// function to write HTML file using the user input
-function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
+// // function to write HTML file using the user input
+// function writeToFile(fileName, data) {
+//   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// }
 
 function initializeProfile() {
   inquirer.prompt(questions).then(inquirerResponses => {
-    console.log('Generating HTML...');
-    writeToFile('profile.html', generateHtml({ ...inquirerResponses }));
+    // console.log('Generating HTML...');
+    // writeToFile('profile.html', generateHtml({ ...inquirerResponses }));
   });
 };
 
-const generateHtml = (name, github) => {
-  return `
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="team-profile-generator/dist/style.css" />
-</head>
-
-<body>
-    <header>
-        <h1>My Team</h1>
-    </header>
-
-    <main class="m-5 row justify-content-around">
-        <div class="col-3">
-            <div class="card">
-                <h3 class="card-header">${name}</h3>
-                <br>
-                <div class="card-body">
-                    <p>${title}</p>
-                    <p>ID #: ${id}</p>
-                    <p><a href="mailto:${email}">Email: ${email}</a></p>
-                    <p>Office #: </p>
-                    <p><a href="https://github.com/${github}">Github</a></p>
-                    <p>School: </p>
-                </div>
-            </div>
-        </div>
-    </main>
-</body>
-  `;
-};
+// generateHtml();
 
 initializeProfile();
