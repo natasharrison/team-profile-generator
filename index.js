1cc44bce3a074c227dd6c901f01069e2ad9107b2
natasharrison <natasharrison@gmail.com>
2021-05-23 15:05:29 -0700

    generateHTML file

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
+// imported required packages
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
-
 const Profile = require('./lib/Profile');
-const generateHtml = require('./dist/generateHtml');
+const generateHtml = require('./src/generateHtml');
 
-
+// array of questions for user input 
 const questions = [
   {
     type: 'input',
     name: 'name',
-    message: 'First and last name of your team member:'
+    message: 'First and last name of your team member:',
+    validate: employeeName => {
+      if (employeeName) {
+        return true;
+      } else {
+        console.log('Please enter the name of your employee!');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'id',
-    message: "Team member's ID number:"
+    message: "Team member's ID number:",
+    validate: employeeID => {
+      if (employeeID) {
+        return true;
+      } else {
+        console.log('Please enter the employee ID number!');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'email',
-    message: "Team member's email address:"
+    message: "Team member's email address:",
+    validate: employeeEmail => {
+      if (employeeEmail) {
+        return true;
+      } else {
+        console.log('Please enter the employee email address!');
+        return false;
+      }
+    }
   },
   {
     type: 'list',
@@ -32,27 +56,54 @@ const questions = [
   {
     type: 'input',
     name: 'office',
-    message: "Team manager's office number:"
+    message: "Team manager's office number:",
+    validate: employeeOffice => {
+      if (employeeOffice) {
+        return true;
+      } else {
+        console.log('Please enter the office number!');
+        return false;
+      }
+    },
+   when: (answers) => answers.role === 'manager'
   },
   // only for engineer
   {
     type: 'input',
     name: 'github',
-    message: "Team engineer's GitHub username:"
+    message: "Team engineer's GitHub username:",
+    validate: employeeGithub => {
+      if (employeeGithub) {
+        return true;
+      } else {
+        console.log('Please enter the employee GitHub username!');
+        return false;
+      }
+    }, 
+    when: (answers) => answers.role === 'engineer'
+
   },
   // only for intern
   {
     type: 'input',
     name: 'school',
-    message: "Team intern's school name:"
-  }, 
+    message: "Team intern's school name:",
+    validate: employeeSchool => {
+      if (employeeSchool) {
+        return true;
+      } else {
+        console.log('Please enter the name of the school!');
+        return false;
+      }
+    }, 
+       when: (answers) => answers.role === 'intern'
+  },
   {
-    type:'confirm', 
+    type: 'confirm',
     name: 'repeat',
-    message: "Would you like to enter another team member?"
-  }
-  // if yes, repeat questions 
-  // if no, generate html
+    message: "Would you like to enter another team member?",
+    default: false
+  }, 
 ];
 
 // function to write HTML file using the user input
@@ -62,8 +113,12 @@ function writeToFile(fileName, data) {
 
 function initializeProfile() {
   inquirer.prompt(questions).then(inquirerResponses => {
+    if (inquirerResponses.repeat === true) {
+      initializeProfile() 
+    } else {
     console.log('Generating HTML...');
-    writeToFile('profile.html', generateHtml({ ...inquirerResponses }));
+    writeToFile('./dist/profile.html', generateHtml({ ...inquirerResponses }));
+    }
   });
 };
 
